4d25eaa047d079b5c9dcad8b80c283cef742cc3c
Andrew Bruce <andrew@camelpunch.com>
2015-05-30 22:59:11 +0100

    cider, clojure-mode, rainbow-delimiters

--- init.el
+++ init.el
@@ -3,3 +3,31 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;;;; package stuff
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(defvar packages
+  '(
+    cider
+    clojure-mode
+    rainbow-delimiters
+    ))
+
+(defun install-packages ()
+  "Install packages that I use"
+  (interactive)
+  (package-refresh-contents)
+  (mapc #'(lambda (package)
+            (unless (package-installed-p package)
+              (package-install package)))
+	packages))
+
+;;;; enable stuff
+
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
