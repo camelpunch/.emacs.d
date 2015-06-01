f1cafffadf0ed778cc0106638b6bac9c62de6149
Andrew Bruce <andrew@camelpunch.com>
2015-06-01 15:57:19 +0100

    Inhibit startup message

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(setq inhibit-startup-message t)
 
 ;;;; package stuff
 (require 'package)
