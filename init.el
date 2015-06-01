cea946309e97fc25e452e401135fa7f49d189f58
Andrew Bruce <andrew@camelpunch.com>
2015-06-01 20:58:02 +0100

    paredit for clojure and repl

--- init.el
+++ init.el
@@ -38,6 +38,8 @@
 (when (package-installed-p 'paredit)
   (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
   (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+  (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
+  (add-hook 'cider-repl-mode-hook       #'enable-paredit-mode)
   (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
   (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
