c8c77b2568e282119fed03b8af35c5b155da9461
Andrew Bruce <me@andrewbruce.net>
2016-11-29 00:00:34 +0000

    more elm config

--- init.el
+++ init.el
@@ -73,6 +73,11 @@
 (add-to-list 'load-path "/Users/pivotal/.opam/system/share/emacs/site-lisp")
 (require 'ocp-indent)
 
+;;;; elm config
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'company-elm))
+(add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
+
 ;;;; editor config
 
 (require 'mouse)
