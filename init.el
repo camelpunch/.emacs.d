69ca7b6d935e5c27b39091943a26b864f7b6c4f9
Andrew Bruce <me@andrewbruce.net>
2016-12-01 00:30:15 +0000

    Haskell 4 indent

--- init.el
+++ init.el
@@ -102,7 +102,12 @@
  ;; If there is more than one, they won't work right.
  '(clojure-align-forms-automatically t)
  '(elm-format-on-save t)
- '(elm-indent-offset 2))
+ '(elm-indent-offset 2)
+ '(haskell-indent-spaces 4)
+ '(haskell-indentation-layout-offset 4)
+ '(haskell-indentation-left-offset 4)
+ '(haskell-indentation-starter-offset 4)
+ '(haskell-stylish-on-save t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
