a9b4bd8e7a8ef40c697bb68320968d5a7d71d1e9
Andrew Bruce <me@andrewbruce.net>
2017-03-20 00:17:18 +0000

    use sendmail

--- init.el
+++ init.el
@@ -113,6 +113,7 @@
  '(package-selected-packages
    (quote
     (magit intero yaml-mode rust-mode rainbow-delimiters paredit haskell-mode fiplr erlang elm-mode company cider)))
+ '(send-mail-function (quote sendmail-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
