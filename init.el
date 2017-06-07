f4a84a235a827d2a60884caa9c72ab2eddd31b11
Andrew Bruce <me@andrewbruce.net>
2017-06-07 21:28:41 +0100

    set larger default font

--- init.el
+++ init.el
@@ -121,7 +121,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#2e3436" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 200 :width normal :foundry "DAMA" :family "Ubuntu Mono")))))
 
 ;;; Fira code
 ;; This works when using emacs --daemon + emacsclient
