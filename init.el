31445d83e61cd4a5b8be63a335d2d1eaf4a59f71
Andrew Bruce <me@andrewbruce.net>
2017-11-23 19:35:08 +0000

    go-mode

--- init.el
+++ init.el
@@ -21,6 +21,7 @@
     erlang
     eziam-theme
     fiplr
+    go-mode
     haskell-mode
     idris-mode
     intero
@@ -80,6 +81,11 @@
 (with-eval-after-load 'company
   (add-to-list 'company-backends 'company-elm))
 (add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
+
+;;;; go config
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook #'gofmt-before-save)
+(setq gofmt-before-save t)
 
 ;;;; haskell config
 (setq haskell-stylish-on-save t)
