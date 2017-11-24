c96a1a2e07dcca2f9c094ae97fe3fa71ba3a3839
Andrew Bruce <me@andrewbruce.net>
2017-11-24 00:53:52 +0000

    go-mode and enable global flycheck

--- init.el
+++ init.el
@@ -56,6 +56,7 @@
   (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
   (add-hook 'cider-repl-mode-hook       #'enable-paredit-mode)
   (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'go-mode-hook               #'enable-paredit-mode)
   (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
@@ -98,6 +99,7 @@
 	    (local-set-key [?\t] (quote idris-simple-indent))))
 
 ;;;; editor config
+(add-hook 'after-init-hook #'global-flycheck-mode)
 (setq linum-format "%d ")
 (require 'mouse)
 (xterm-mouse-mode t)
