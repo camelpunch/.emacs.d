b9f797b7bcb400f25e112ff7e5fb9c7026de29fb
Andrew Bruce <me@andrewbruce.net>
2018-04-22 19:56:14 +0100

    tide and typescript config

--- init.el
+++ init.el
@@ -36,6 +36,7 @@
     rainbow-delimiters
     rust-mode
     terraform-mode
+    tide
     yaml-mode
     ))
 
@@ -98,6 +99,27 @@
 
 ;;;; terraform config
 (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+
+;;;; typescript config
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 ;;;; haskell config
 (setq haskell-stylish-on-save t)
