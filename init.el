6efc75265d344724ed9fd0b1b801ed9e8d7e8be3
Andrew Bruce <me@andrewbruce.net>
2018-11-29 08:54:51 +0000

    Add Source Code Pro font

--- init.el
+++ init.el
@@ -224,7 +224,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Source Code Variable" :foundry "ADBO" :slant normal :weight normal :height 143 :width normal)))))
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
