c9a0f5a668b860a3bce98651f22e2ceffb936d25
Andrew Bruce <me@andrewbruce.net>
2019-02-03 12:06:51 +0000

    Tidy config and remove nonsense

--- init.el
+++ init.el
@@ -5,18 +5,7 @@
 
 ;;; Code:
 
-(setenv "SSH_AUTH_SOCK"
-        "/run/user/1000/gnupg/S.gpg-agent.ssh")
-
-(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(setq column-number-mode t
-      inhibit-startup-echo-area-message t
-      initial-scratch-message nil
-      inhibit-startup-message t)
-
-;;;; packages
+;; packages
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
@@ -32,6 +21,7 @@
     git-gutter
     haskell-mode
     hasklig-mode
+    hlint-refactor
     idris-mode
     intero
     magit
@@ -52,81 +42,14 @@
               (package-install package)))
 	config-packages))
 
-;;;; enable packages
+;; use gpg-agent as SSH agent
+(setenv "SSH_AUTH_SOCK"
+        "/run/user/1000/gnupg/S.gpg-agent.ssh")
 
-(when (package-installed-p 'exec-path-from-shell)
-  (add-hook 'after-init-hook 'exec-path-from-shell-initialize))
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-(when (package-installed-p 'git-gutter)
-  (add-hook 'after-init-hook 'global-git-gutter-mode))
-
-(when (package-installed-p 'company)
-  (add-hook 'after-init-hook 'global-company-mode))
-
-(when (package-installed-p 'paredit)
-  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
-  (add-hook 'haskell-mode-hook          #'enable-paredit-mode)
-  (add-hook 'idris-mode-hook            #'enable-paredit-mode))
-
-(when (package-installed-p 'rainbow-delimiters)
-  (require 'rainbow-delimiters)
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
-
-(when (package-installed-p 'intero)
-  (add-hook 'haskell-mode-hook 'intero-mode))
-(when (and (package-installed-p 'hasklig-mode)
-           (display-graphic-p))
-  (add-hook 'haskell-mode-hook 'hasklig-mode)
-  (add-hook 'idris-mode-hook 'hasklig-mode))
-
-;;;; terraform config
-(add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
-
-;; aligns annotation to the right hand side
-(defvar company-tooltip-align-annotations)
-(setq company-tooltip-align-annotations t)
-
-;;;; haskell config
-(defvar haskell-stylish-on-save)
-(setq haskell-stylish-on-save t)
-
-(defvar haskell-mode-stylish-haskell-path)
-(setq haskell-mode-stylish-haskell-path "brittany")
-(with-eval-after-load 'intero
-  (flycheck-add-next-checker 'intero '(warning . haskell-hlint))
-  )
-(set-face-attribute 'default nil
-                    :family "Hasklig"
-                    :height 200
-                    :weight 'normal
-                    :width 'normal)
-
-;;;; idris config
-(add-hook 'idris-mode-hook
-	  (lambda ()
-	    (local-set-key [backtab] (quote idris-simple-indent-backtab))
-	    (local-set-key [?\t] (quote idris-simple-indent))))
-
-;;;; editor config
-
-;; no tabs!
-(setq-default indent-tabs-mode nil)
-
-;; faster autocomplete
-(defvar company-idle-delay)
-(setq company-idle-delay 0.1)
-
-;; show filename in title bar regardless of numbers of frames
-(setq frame-title-format "%b")
-
-;; the rest
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
 (blink-cursor-mode 0)
 (winner-mode 1)
 (savehist-mode 1)
@@ -139,12 +62,103 @@
 (setq ring-bell-function 'ignore)
 (global-auto-revert-mode t)
 
+;; font
+(set-face-attribute 'default nil
+                    :family "Hasklig"
+                    :height 200
+                    :weight 'normal
+                    :width 'normal)
+(when (and (package-installed-p 'hasklig-mode)
+           (display-graphic-p))
+  (add-hook 'prog-mode-hook 'hasklig-mode))
+(custom-set-faces
+ '(font-lock-function-name-face ((t (:height 1.0)))))
+
+;; no tabs!
+(setq-default indent-tabs-mode nil)
+
+;; show filename in title bar regardless of numbers of frames
+(setq frame-title-format "%b")
+
+;; all programming languages
+(add-hook 'prog-mode-hook
+          ;; highlight parens
+          'show-paren-mode)
+
+(add-hook 'before-save-hook
+          'delete-trailing-whitespace)
+
+(defvar company-idle-delay) ; autocomplete delay
+(setq company-idle-delay 0.1
+      indent-tabs-mode nil
+      column-number-mode t
+      inhibit-startup-echo-area-message t
+      initial-scratch-message nil
+      inhibit-startup-message t)
+
+;; mouse
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-follow-mouse 't)
 (setq scroll-step 1)
 (global-set-key (kbd "<mouse-6>") #'ignore)
 (global-set-key (kbd "<mouse-7>") #'ignore)
 
+;; copy PATH from shell
+(when (package-installed-p 'exec-path-from-shell)
+  (add-hook 'after-init-hook 'exec-path-from-shell-initialize))
+
+;; git gutter
+(when (package-installed-p 'git-gutter)
+  (add-hook 'after-init-hook 'global-git-gutter-mode))
+
+;; autocompletion
+(when (package-installed-p 'company)
+  (add-hook 'after-init-hook 'global-company-mode))
+
+;; paredit
+(when (package-installed-p 'paredit)
+  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+  (add-hook 'haskell-mode-hook          #'enable-paredit-mode)
+  (add-hook 'idris-mode-hook            #'enable-paredit-mode))
+
+;; rainbow parens
+(when (package-installed-p 'rainbow-delimiters)
+  (require 'rainbow-delimiters)
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
+
+;; terraform
+(add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+
+;; align autocomplete tooltips to the right hand side
+(defvar company-tooltip-align-annotations)
+(setq company-tooltip-align-annotations t)
+
+;; haskell
+(defvar haskell-stylish-on-save)
+(setq haskell-stylish-on-save t)
+
+(defvar haskell-mode-stylish-haskell-path)
+(setq haskell-mode-stylish-haskell-path "brittany")
+
+(when (package-installed-p 'intero)
+  (add-hook 'haskell-mode-hook 'intero-mode))
+(with-eval-after-load 'intero
+  (flycheck-add-next-checker 'intero '(warning . haskell-hlint))
+  )
+(haskell-indentation-mode -1)
+
+;; idris
+(add-hook 'idris-mode-hook
+	  (lambda ()
+	    (local-set-key [backtab] (quote idris-simple-indent-backtab))
+	    (local-set-key [?\t] (quote idris-simple-indent))))
+
+;; projectile fuzzy find
 (when (package-installed-p 'projectile)
   (projectile-mode +1)
   (recentf-mode +1)
@@ -152,26 +166,31 @@
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
+;; magit
 (global-set-key (kbd "C-x g") 'magit-status)
-(add-hook 'prog-mode-hook 'show-paren-mode)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
  '(blink-cursor-mode nil)
- '(custom-enabled-themes (quote (dracula)))
  '(case-replace nil)
+ '(company-idle-delay 0.1)
+ '(company-tooltip-align-annotations t)
+ '(custom-enabled-themes (quote (manoj-dark)))
  '(custom-safe-themes
    (quote
     ("aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" "4bfced46dcfc40c45b076a1758ca106a947b1b6a6ff79a3281f3accacfb3243c" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" default)))
  '(fci-rule-color "#383838")
  '(flycheck-global-modes (quote (not idris-mode)))
+ '(global-company-mode t)
  '(global-flycheck-mode t)
- '(haskell-indentation-layout-offset 2)
- '(haskell-indentation-left-offset 2)
- '(haskell-indentation-starter-offset 2)
+ '(haskell-literate-default (quote bird))
  '(haskellindent-spaces 2)
  '(idris-stay-in-current-window-on-compiler-error t)
  '(intero-extra-ghc-options (quote ("-W" "-XOverloadedStrings")))
@@ -186,16 +205,7 @@
      (idris-load-packages "mrk")
      (intero-targets "release:lib" "release:exe:release-exe" "release:test:release-test")
      (idris-load-packages "contrib"))))
- '(send-mail-function (quote sendmail-send-it))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Variable" :foundry "ADBO" :slant normal :weight normal :height 128 :width normal)))))
- '(font-lock-function-name-face ((t (:foreground "mediumspringgreen" :weight bold :height 1.0))))
+)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
